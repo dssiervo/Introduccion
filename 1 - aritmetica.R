a4a84687a70ca187ccc3a787c0633ec3ba28bd9b
dssiervo <123518873+dssiervo@users.noreply.github.com>
2023-01-24 18:54:16 -0500

    Add files via upload

---
+++ 1 - aritmetica.R
@@ -0,0 +1,33 @@
+# OPERACIONES ARITMÉTICAS
+
+5.2+3       # Suma
+4-55        # Resta
+3*-7        # Multiplicación
+21/3        # División
+# No existe la división inversa
+9%%4         # Residuo
+9%/%4       # División truncada
+# No existe una función de base de Cociente hacia abajo 
+# No existe una función de base de Cociente hacia arriba
+
+# No existe una función de base MCM
+# No existe una función de base MCD
+abs(-32)    # Valor absoluto
+sign(-32)   # Signo
+
+
+3^2         # Potencia. No admite **
+sqrt(17)    # Raiz cuadrada
+# No existe una función de base de Raiz cúbica
+exp(1)      # Exponencial
+log(exp(1)) # Logaritmo natural
+log10(1000) # Logaritmo base 10
+log2(16)    # Logaritmo base 2
+log(27, base=3)  # Logaritmo con base arbitraria
+
+# Funciondes de redondeo
+round(pi)
+floor(pi)
+ceiling(pi)
+trunc(pi)
+round(-pi)
---
+++ 1 - aritmetica.jl
@@ -0,0 +1,35 @@
+# OPERACIONES ARITMÉTICAS
+
+5.2+3       # Suma
+@printf("5.2+3=%d\n", (5.2+3))
+println("5.2+3=", (5.2+3))
+4-55        # Resta
+3*-7        # Multiplicación
+21/3        # División
+3\21        # División inversa
+8%3         # Residuo
+div(8,3)    # División truncada
+fld(8,3)    # Cociente hacia abajo 
+cld(8,3)    # Cociente hacia arriba
+
+lcm(2,3,18) # MCM
+gcd(18,66)  # MCD
+abs(-32)    # Valor absoluto
+sign(-32)   # Signo
+
+
+3^2         # Potencia. No admite **
+sqrt(17)    # Raiz cuadrada
+cbrt(-8)    # Raiz cúbica
+exp(1)      # Exponencial
+log(exp(1)) # Logaritmo natural
+log10(1000) # Logaritmo base 10
+log2(16)    # Logaritmo base 2
+log(3, 27)  # Logaritmo con base arbitraria
+
+# Funciondes de redondeo
+round(pi)
+floor(pi)
+ceil(pi)
+trunc(pi)
+round(-pi)
